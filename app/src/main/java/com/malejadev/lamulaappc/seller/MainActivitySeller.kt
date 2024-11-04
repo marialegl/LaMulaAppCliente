f8d4fbb6041349a3afd55b092abd2d43a59b7984
marialegl <psico.marialegl@gmail.com>
2024-11-03 20:01:19 -0500

    Update nav seller

---
+++ app/src/main/java/com/malejadev/lamulaappc/seller/MainActivitySeller.kt
@@ -0,0 +1,17 @@
+package com.malejadev.lamulaappc.seller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.malejadev.lamulaappc.R
+
+class MainActivitySeller : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_seller)
+
+        }
+    }
